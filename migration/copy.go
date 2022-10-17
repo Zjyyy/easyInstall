e643edf8b9749fc6ef53a82c7ac09a858e9e6b75
Zhujy <void_1912@yeah.net>
2022-10-17 22:25:40 +0800

    完成了拷贝目录函数

--- migration/copy.go
+++ migration/copy.go
@@ -1,11 +1,13 @@
 package migration
 
 import (
+	"archive/zip"
 	"easyInstall/conf"
 	"errors"
 	"io"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -45,10 +47,14 @@ func (self *fileMigration) CopyMigration() error {
 		}
 		if !item.IsDirectory {
 			self.copyFile(item.Source, item.Target+"/"+item.FileName)
+		} else {
+			self.copyDirectory(item.Source, item.Target)
 		}
 	}
 	return nil
 }
+
+// 验证配置文件中的路径是否合法有效
 func (self fileMigration) validityPathConfig(conf *conf.DirectoryConf) bool {
 	isSetupFileExist := self.ExistFileOrDirectory(conf.Source)
 	if !isSetupFileExist {
@@ -77,10 +83,100 @@ func (self fileMigration) validityPathConfig(conf *conf.DirectoryConf) bool {
 	}
 	return true
 }
+func (self fileMigration) zipDir(dirPath string, zipFilePath string) {
+	zipFile := filepath.Join(zipFilePath, "/", filepath.Base(zipFilePath)+".zip")
+	log.Println(">>>>> " + zipFile)
+
+	fz, err := os.Create(zipFile)
+	if err != nil {
+		log.Fatalf("创建zip文件失败：%v \n", err.Error())
+	}
+	defer fz.Close()
+
+	w := zip.NewWriter(fz)
+	defer w.Close()
+
+	filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			fDest, err := w.Create(path[len(dirPath)+1:])
+			if err != nil {
+				log.Fatalf("创建zip目标文件失败: %v", err.Error())
+				return nil
+			}
+
+			fSrc, err := os.Open(path)
+			if err != nil {
+				log.Fatalf("创建Zip源头文件失败: %v", err.Error())
+				return nil
+			}
+			defer fSrc.Close()
+			_, err = io.Copy(fDest, fSrc)
+			if err != nil {
+				log.Fatalf("Zip复制失败: %v", err.Error())
+				return nil
+			}
+		}
+		return nil
+	})
+}
+func (self fileMigration) unzipDir(zipFile string, dirPath string) {
+	r, err := zip.OpenReader(zipFile)
+	if err != nil {
+		log.Fatalf("Open zip file failed: %s\n", err.Error())
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+		func() {
+			path := dirPath + string(filepath.Separator) + f.Name
+			os.MkdirAll(filepath.Dir(path), 0755)
+			fDest, err := os.Create(path)
+			if err != nil {
+				log.Printf("Create failed: %s\n", err.Error())
+				return
+			}
+			defer fDest.Close()
+
+			fSrc, err := f.Open()
+			if err != nil {
+				log.Printf("Open failed: %s\n", err.Error())
+				return
+			}
+			defer fSrc.Close()
+
+			_, err = io.Copy(fDest, fSrc)
+			if err != nil {
+				log.Printf("Copy failed: %s\n", err.Error())
+				return
+			}
+		}()
+	}
+}
 func (self fileMigration) copyDirectory(srcDirectoryPath string, destDirectoryPath string) error {
+	filepath.Walk(srcDirectoryPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Fatalf("遍历文件目录错误: %v", err)
+			return nil
+		}
+		if !info.IsDir() {
+			destPath := strings.Replace(path, filepath.Clean(srcDirectoryPath), filepath.Clean(destDirectoryPath), -1)
+			self.copyFile(path, destPath)
+		}
+		return nil
+	})
 	return nil
 }
-func (self fileMigration) copyFile(srcFilePath string, destPath string) error {
+
+// @description 分段拷贝文件
+// @param srcFilePath "源头文件的路径"
+// @param destFilePath "目标文件的路径(具体到文件名)"
+func (self fileMigration) copyFile(srcFilePath string, destFilePath string) error {
+	targetPath := filepath.Dir(destFilePath)
+	// 如果文件的路径不存在，创建路径
+	if !self.ExistFileOrDirectory(targetPath) {
+		os.MkdirAll(targetPath, os.ModePerm)
+	}
+
 	srcFile, err := os.Open(srcFilePath)
 	if err != nil {
 		log.Fatalf("读取文件错误：%v \n", err.Error())
@@ -89,7 +185,7 @@ func (self fileMigration) copyFile(srcFilePath string, destPath string) error {
 	defer srcFile.Close() // close after checking err
 
 	// 创建目标文件，稍后会向这个目标文件写入拷贝内容
-	distFile, err := os.Create(destPath)
+	distFile, err := os.Create(destFilePath)
 	if err != nil {
 		log.Fatalf("目标文件创建失败，原因是: %v \n", err)
 	}
@@ -118,6 +214,7 @@ func (self fileMigration) copyFile(srcFilePath string, destPath string) error {
 	return nil
 }
 
+// @description 判断文件或者目录是否存在
 func (self fileMigration) ExistFileOrDirectory(filePath string) bool {
 	_, err := os.Stat(filePath)
 	if err != nil {
@@ -129,6 +226,7 @@ func (self fileMigration) ExistFileOrDirectory(filePath string) bool {
 	return true
 }
 
+// @description 判断是否是目录
 func (self fileMigration) IsDirectory(directoryPath string) bool {
 	s, err := os.Stat(directoryPath)
 	if err != nil {
@@ -137,6 +235,7 @@ func (self fileMigration) IsDirectory(directoryPath string) bool {
 	return s.IsDir()
 }
 
+// @description 判断是否是文件
 func (self fileMigration) IsFile(filePath string) bool {
 	return !self.IsDirectory(filePath)
 }
